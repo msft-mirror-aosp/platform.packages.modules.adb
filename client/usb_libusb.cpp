7f647f361847250cfc5904a31c4c597b19de847e
Fabien Sanglard <sanglardf@google.com>
2024-09-30 18:50:03 -0700

    Delete dead code
    Test: NA
    Bug: NA
    Change-Id: Ie796a32e971eeda1496600876199c7dd47252adb

--- client/usb_libusb.cpp
+++ client/usb_libusb.cpp
@@ -722,7 +722,6 @@ struct LibusbConnection : public Connection {
             });
 
             incoming_header_.reset();
-            incoming_payload_.clear();
         }
 
         if (device_handle_) {
@@ -876,7 +875,6 @@ struct LibusbConnection : public Connection {
     ReadBlock header_read_ GUARDED_BY(read_mutex_);
     ReadBlock payload_read_ GUARDED_BY(read_mutex_);
     std::optional<amessage> incoming_header_ GUARDED_BY(read_mutex_);
-    IOVector incoming_payload_ GUARDED_BY(read_mutex_);
 
     std::mutex write_mutex_;
     std::unordered_map<TransferId, std::unique_ptr<WriteBlock>> writes_ GUARDED_BY(write_mutex_);
